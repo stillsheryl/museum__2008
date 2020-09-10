183f63e7b462b73272d6d06cbb374af173ed9ac7
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-10 10:07:35 -0600

    Add test can add patrons

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,11 +1,29 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    recommendations = []
+    @exhibits.find_all do |exhibit|
+      patron.interests.find_all do |interest|
+        if interest == exhibit.name
+          recommendations << exhibit
+        end
+      end
+      recommendations
+    end
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -37,6 +37,9 @@ class MuseumTest < Minitest::Test
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
     patron_1 = Patron.new("Bob", 20)
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
@@ -45,6 +48,28 @@ class MuseumTest < Minitest::Test
 
     assert_equal [gems_and_minerals, dead_sea_scrolls], dmns.recommend_exhibits(patron_1)
     assert_equal [imax], dmns.recommend_exhibits(patron_2)
+  end
+
+  def test_it_can_show_patrons_by_exhibit_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    assert_equal [patron_1, patron_2, patron_3], dmns.patrons
   end
 
 end
