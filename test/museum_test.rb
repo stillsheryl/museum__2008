ce653327409d34c5e5536eff4264af807dfd891b
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-10 09:32:16 -0600

    Add test can recommend exhibits to patreons

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/museum'
+require './lib/exhibit'
+require './lib/patron'
 
 class MuseumTest < Minitest::Test
 
@@ -28,6 +30,21 @@ class MuseumTest < Minitest::Test
     dmns.add_exhibit(imax)
 
     assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+  end
+
+  def test_can_recommend_exhibits_to_patrons
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls], dmns.recommend_exhibits(patron_1)
+    assert_equal [imax], dmns.recommend_exhibits(patron_2)
   end
 
 end
